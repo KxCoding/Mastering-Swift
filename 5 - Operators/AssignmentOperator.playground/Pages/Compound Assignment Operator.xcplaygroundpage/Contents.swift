2158f94e7a5ace3c0099e28b1609ae6da465f3fb
Keun young Kim <kky0317@gmail.com>
2018-12-12 17:22:39 +0900

    Chapter 5, 6, 7, 8

---
+++ 5 - Operators/AssignmentOperator.playground/Pages/Compound Assignment Operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,137 @@
+//
+//  Copyright (c) 2018 KxCoding <kky0317@gmail.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Compound Assignment Operators
+ */
+
+/*:
+ ## Addition Assignment Operator
+ ````
+ a += b
+ a = a + b
+ ````
+ */
+
+
+
+/*:
+ ## Subtraction Assignment Operator
+ ````
+ a -= b
+ a = a - b
+ ````
+ */
+
+
+
+/*:
+ ## Multiplication Assignment Operator
+ ````
+ a *= b
+ a = a * b
+ ````
+ */
+
+
+
+/*:
+ ## Division Assignment Operator
+ ````
+ a /= b
+ a = a / b
+ ````
+ */
+
+
+
+/*:
+ ## Modulo Assignment Operator
+ ````
+ a %= b
+ a = a % b
+ ````
+ */
+
+
+/*:
+ ## Bitwise AND Assignment Operator
+ ````
+ a &= b
+ a = a & b
+ ````
+ */
+
+
+
+/*:
+ ## Bitwise OR Assignment Operator
+ ````
+ a |= b
+ a = a | b
+ ````
+ */
+
+
+
+/*:
+ ## Bitwise XOR Assignment Operator
+ ````
+ a ^= b
+ a = a ^ b
+ ````
+ */
+
+
+
+/*:
+ ## Bitwise Left Shift Assignment Operator
+ ````
+ a <<= b
+ a = a << b
+ ````
+ */
+
+
+
+/*:
+ ## Bitwise Right Shift Assignment Operator
+ ````
+ a >>= b
+ a = a >> b
+ ````
+ */
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 5 - Operators/ShortCircuitEvaluation.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+/*:
+ # Short-circuit Evaluation
+ */
+
+
+
+
+
