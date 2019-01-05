deaa6866c34d61925fd4893ba4a2cb846bef3c31
Keun young Kim <kky0317@gmail.com>
2019-01-05 18:09:55 +0900

    Chapter 11, 12, 13

---
+++ 11-Closures/Closures.playground/Pages/case1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let products = [
+   "MacBook Air", "MacBook Pro",
+   "iMac", "iMac Pro", "Mac Pro", "Mac mini",
+   "iPad Pro", "iPad", "iPad mini",
+   "iPhone Xs", "iPhone Xr", "iPhone 8", "iPhone 7",
+   "AirPods",
+   "Apple Watch Series 4", "Apple Watch Nike+"
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+//: [Next](@next)
