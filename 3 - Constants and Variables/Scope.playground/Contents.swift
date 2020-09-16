dbc8a35439e454ff06e5cb4a7da40a8a7c3f7ab2
kei <kky0317@gmail.com>
2020-09-16 14:11:53 +0900

    update chapter 3

--- 3 - Constants and Variables/Scope.playground/Contents.swift
+++ 3 - Constants and Variables/Scope.playground/Contents.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2018 KxCoding <kky0317@gmail.com>
+//  Copyright (c) KxCoding <kky0317@gmail.com>
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
