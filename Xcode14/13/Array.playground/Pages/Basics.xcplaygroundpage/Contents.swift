a5a7d5e1260236efbffbad035532c11854de4266
kei <kky0317@gmail.com>
2023-03-27 18:05:14 +0900

    add xcode 14 version

---
+++ Xcode14/13/Array.playground/Pages/Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,149 @@
+/*
+ Mastering Swift
+ Copyright (c) 2022 KxCoding
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Array Basics
+ ## Creating an Array
+ */
+
+
+
+
+
+
+
+
+
+
+/*:
+ ## Inspecting an Array
+ */
+
+
+
+
+
+
+
+
+
+
+/*:
+ ## Accessing Elements
+ */
+
+
+
+
+
+
+
+
+
+
+/*:
+ ## Adding Elements
+ */
+
+
+
+
+
+
+
+
+
+
+/*:
+ ## Removing Elements
+ */
+
+var alphabet = ["A", "B", "C", "D", "E", "F", "G"]
+
+
+
+
+
+
+
+
+
+
+/*:
+ ## Comparing Arrays
+ */
+
+let a = ["A", "B", "C"]
+let b = ["a", "b", "c"]
+
+
+
+
+
+
+
+
+
+
+/*:
+ ## Finding Elements
+ */
+
+let randomNumbers = [1, 2, 3, 1, 4, 5, 2, 6, 7, 5, 0]
+
+
+
+
+
+
+
+
+
+
+/*:
+ ## Sorting on Array
+ */
+
+
+
+
+
+
+
+
+
+
+/*:
+ ## Transforming an Array
+ */
+
+let evenNumbers = [2, 4, 6, 8, 10]
+let multiDimension = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+let optionalNums = [1, 2, 3, nil, 5, 6, 7, 8, nil]
+let words = ["Hello", "Swift", "Programming"]
+let multiline = """
+Hello, Swift
+Hello, iOS
+Goodbye, Objective-C
+"""
+
+
+
+
+
+
+
+
+
+
---
+++ Xcode14/26/CustomOperators.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,54 @@
+/*
+ Mastering Swift
+ Copyright (c) 2022 KxCoding
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+import UIKit
+
+/*:
+ # Custom Operators
+ ![operator](operator.png)
+ 
+ ### Reserved Tokens
+ (, ), {, }, [, ], ., ,, :, ;, =, @, #, &(prefix operator), ->, `, ?, !(postfix operator), \/*, *\/
+                                                                                             
+                                                                                             ### First Character
+                                                                                             /, =, -, +, !, *, %, <, >, &, |, ^, ?, ~
+                                                                                             
+                                                                                             ![custom-operator](custom-operator.png)
+                                                                                             */*/
+
+
+
+
+
+
+
+
+
+
+
+/*:
+ # Precedence Groups
+ ![precedence-group](precedence-group.png)
+ 
+ [Operator Declarations]:
+ https://developer.apple.com/documentation/swift/operator_declarations "Operator Declarations"
+ 
+ [Operator Declarations]
+ 
+ ![precedence-group-syntax](precedence-group-syntax.png)
+ 
+ ![precedence-group-declaration](precedence-group-declaration.png)
+ */
+
+
+
+
+
+
